32c4044e1464f95d634c0eb3f1e7289165a1d8c0
mateus <mateusv@email.com>
2019-05-24 23:22:39 -0300

    URI1060

---
+++ URI1060.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int cont = 0;
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 6; i++){
+            if (input.nextDouble() > 0){
+                cont += 1;
+            }
+        }
+        System.out.print(cont);
+        System.out.print(" valores positivos\n");
+    }
+} 
