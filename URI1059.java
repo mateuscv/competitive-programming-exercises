704da9f7aa026295846b31656e92a11d4a48d2b8
mateus <mateusv@email.com>
2019-05-24 22:06:08 -0300

    URI1059

---
+++ URI1059.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i%2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
