8248e0af9ea63a49e8920f02188ce897969a9481
mateus <mateusv@email.com>
2020-02-06 10:11:22 -0300

    Organising in folders.

--- URI/URI1060.java
+++ URI/URI1060.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class URI1060 {
     public static void main(String[] args) {
         int cont = 0;
         Scanner input = new Scanner(System.in);
