fb3b9a04136b089f4d98c120f6d9e5850c8f4392
mateus <mateusv@email.com>
2020-03-21 17:48:15 -0300

    86 was missing solution

--- Neps/86.cpp
+++ Neps/86.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
 using namespace std;
+
+int main(){
+    double A, B, media;
+    cin >> A >> B;
+    media = ((A+B)/2);
+    if (media >= 7.0)
+        cout << "Aprovado";
+    else if (media >= 4) cout << "Recuperacao";
+    else cout << "Reprovado";
+    return 0;
+}
