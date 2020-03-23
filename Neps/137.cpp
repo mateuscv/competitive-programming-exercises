ccf4bffe802fc898bef3351e687c58824ee05a78
mateus <mateusv@email.com>
2020-03-23 11:26:36 -0300

    137

---
+++ Neps/137.cpp
@@ -0,0 +1,13 @@
+#define PA 4
+#define PB 6
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A,B;
+    cin >> A;
+    cin >> B;
+    cout << ((A*PA + B * PB)/10);
+    return 0;
+}
