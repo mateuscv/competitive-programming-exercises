c70aebe6a5d732a83c3db5c604098e2665715c43
mateus <mateusv@email.com>
2020-03-25 10:03:00 -0300

    1564

---
+++ URI/1564.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N;
+    while (cin >> N){
+        if(N==0){
+            cout << "vai ter copa!" << "\n";
+        } else cout << "vai ter duas!" << "\n";
+    }
+    
+    return 0;
+}
